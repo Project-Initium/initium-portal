9b289dd9b7357daf5e964c3bbe8f6495538e4ead
Andrew Davis <andrew@deviouscreation.com>
2020-02-03 21:37:47 +0000

    Start of implementation

---
+++ Source/Stance.Core/Domain/ErrorCodes.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) DeviousCreation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+namespace Stance.Core.Domain
+{
+    public enum ErrorCodes
+    {
+        SavingChanges = 1,
+        SystemIsAlreadySetup = 2,
+    }
+}
---
+++ Source/Stance.Core/Domain/ErrorData.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) DeviousCreation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+namespace Stance.Core.Domain
+{
+    public sealed class ErrorData
+    {
+        public ErrorData(ErrorCodes code, string message)
+        {
+            this.Code = code;
+            this.Message = message;
+        }
+
+        public ErrorData(ErrorCodes code)
+            : this(code, code.ToString())
+        {
+        }
+
+        public ErrorCodes Code { get; }
+
+        public string Message { get; }
+    }
+}
