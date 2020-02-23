d6f8458b82d4a73a762def1fa8eec7dac2e517b0
Andrew Davis <andrew@deviouscreation.com>
2020-02-23 23:11:16 +0000

    Adding simple tests to cover the new feature

---
+++ Tests/Stance.Tests/Web/Components/LeftNav/LeftNavViewComponentTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) DeviousCreation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Stance.Web.Components.LeftNav;
+using Xunit;
+
+namespace Stance.Tests.Web.Components.LeftNav
+{
+    public class LeftNavViewComponentTests
+    {
+        [Fact]
+        public void Invoke_GivenNoArguments_ExpectView()
+        {
+            var leftNavViewComponent = new LeftNavViewComponent();
+            var result = leftNavViewComponent.Invoke();
+            Assert.IsType<ViewViewComponentResult>(result);
+        }
+    }
+}
---
+++ Tests/Stance.Tests/Web/Components/LeftNav/TopNavViewComponentTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) DeviousCreation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Stance.Web.Components.TopNav;
+using Xunit;
+
+namespace Stance.Tests.Web.Components.LeftNav
+{
+    public class TopNavViewComponentTests
+    {
+        [Fact]
+        public void Invoke_GivenNoArguments_ExpectView()
+        {
+            var topNavViewComponent = new TopNavViewComponent();
+            var result = topNavViewComponent.Invoke();
+            Assert.IsType<ViewViewComponentResult>(result);
+        }
+    }
+}
