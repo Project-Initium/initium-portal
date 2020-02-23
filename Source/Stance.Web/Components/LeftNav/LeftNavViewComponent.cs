2e3e4f0b1ca4db295bd0967219b38cc542f88dcb
Andrew Davis <andrew@deviouscreation.com>
2020-02-23 22:38:31 +0000

    Updating code based on sonar refactorings

--- Source/Stance.Web/Components/LeftNav/LeftNavViewComponent.cs
+++ Source/Stance.Web/Components/LeftNav/LeftNavViewComponent.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// Copyright (c) DeviousCreation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace Stance.Web.Components.LeftNav
 {
