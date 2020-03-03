4e7c2aef6252bbfc24941431fd813eef75506aa6
Andrew Davis <andrew@deviouscreation.com>
2020-03-03 22:53:50 +0000

    Adding commands to register mfa items have been requested

---
+++ Source/Stance.Domain/CommandHandlers/UserAggregate/AppMfaRequestedCommandHandler.cs
@@ -0,0 +1,68 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using NodaTime;
+using ResultMonad;
+using Stance.Core.Constants;
+using Stance.Core.Contracts;
+using Stance.Core.Domain;
+using Stance.Domain.AggregatesModel.UserAggregate;
+using Stance.Domain.Commands.UserAggregate;
+
+namespace Stance.Domain.CommandHandlers.UserAggregate
+{
+    public class AppMfaRequestedCommandHandler : IRequestHandler<AppMfaRequestedCommand, ResultWithError<ErrorData>>
+    {
+        private readonly IClock _clock;
+        private readonly ICurrentAuthenticatedUserProvider _currentAuthenticatedUserProvider;
+        private readonly IUserRepository _userRepository;
+
+        public AppMfaRequestedCommandHandler(
+            IUserRepository userRepository, ICurrentAuthenticatedUserProvider currentAuthenticatedUserProvider,
+            IClock clock)
+        {
+            this._userRepository = userRepository;
+            this._currentAuthenticatedUserProvider = currentAuthenticatedUserProvider;
+            this._clock = clock;
+        }
+
+        public async Task<ResultWithError<ErrorData>> Handle(
+            AppMfaRequestedCommand request, CancellationToken cancellationToken)
+        {
+            var result = await this.Process(cancellationToken);
+            var dbResult = await this._userRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+
+            if (!dbResult)
+            {
+                return ResultWithError.Fail(new ErrorData(
+                    ErrorCodes.SavingChanges, "Failed To Save Database"));
+            }
+
+            return result;
+        }
+
+        private async Task<ResultWithError<ErrorData>> Process(CancellationToken cancellationToken)
+        {
+            var currentUserMaybe = this._currentAuthenticatedUserProvider.CurrentAuthenticatedUser;
+            if (currentUserMaybe.HasNoValue)
+            {
+                return ResultWithError.Fail(new ErrorData(ErrorCodes.UserNotFound));
+            }
+
+            var userMaybe = await this._userRepository.Find(currentUserMaybe.Value.UserId, cancellationToken);
+
+            if (userMaybe.HasNoValue)
+            {
+                return ResultWithError.Fail(new ErrorData(ErrorCodes.UserNotFound));
+            }
+
+            var user = userMaybe.Value;
+
+            user.ProcessPartialSuccessfulAuthenticationAttempt(
+                this._clock.GetCurrentInstant().ToDateTimeUtc(),
+                AuthenticationHistoryType.AppMfaRequested);
+
+            return ResultWithError.Ok<ErrorData>();
+        }
+    }
+}
---
+++ Tests/Stance.Tests/Domain/CommandHandlers/UserAggregate/AppMfaRequestedCommandHandlerTests.cs
@@ -0,0 +1,308 @@
+﻿// Copyright (c) DeviousCreation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MaybeMonad;
+using Moq;
+using NodaTime;
+using Stance.Core;
+using Stance.Core.Constants;
+using Stance.Core.Contracts;
+using Stance.Core.Contracts.Domain;
+using Stance.Core.Domain;
+using Stance.Domain.AggregatesModel.UserAggregate;
+using Stance.Domain.CommandHandlers.UserAggregate;
+using Stance.Domain.Commands.UserAggregate;
+using Xunit;
+
+namespace Stance.Tests.Domain.CommandHandlers.UserAggregate
+{
+    public class AppMfaRequestedCommandHandlerTests
+    {
+        [Fact]
+        public async Task Handle_GivenNoUserAppearsToBeAuthenticate_ExpectFailedResultAndNoAttemptLogged()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => true);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe.From(user.Object));
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe<AuthenticatedUser>.Nothing);
+
+            var clock = new Mock<IClock>();
+
+            var handler = new AppMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new AppMfaRequestedCommand();
+
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorCodes.UserNotFound, result.Error.Code);
+            user.Verify(
+                x => x.ProcessPartialSuccessfulAuthenticationAttempt(
+                    It.IsAny<DateTime>(), It.IsAny<AuthenticationHistoryType>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_GivenSavingFails_ExpectFailedResult()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => false);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe.From(user.Object));
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe.From(new AuthenticatedUser(Guid.NewGuid())));
+
+            var clock = new Mock<IClock>();
+
+            var handler = new AppMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new AppMfaRequestedCommand();
+
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorCodes.SavingChanges, result.Error.Code);
+        }
+
+        [Fact]
+        public async Task Handle_GivenSavingSucceeds_ExpectSuccessfulResult()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => true);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe.From(user.Object));
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe.From(new AuthenticatedUser(Guid.NewGuid())));
+
+            var clock = new Mock<IClock>();
+
+            var handler = new AppMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new AppMfaRequestedCommand();
+
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task Handle_GivenUserDoesExist_ExpectSuccessfulResultAndAttemptLogged()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => true);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe.From(user.Object));
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe.From(new AuthenticatedUser(Guid.NewGuid())));
+
+            var clock = new Mock<IClock>();
+
+            var handler = new AppMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new AppMfaRequestedCommand();
+
+            await handler.Handle(cmd, CancellationToken.None);
+            user.Verify(
+                x => x.ProcessPartialSuccessfulAuthenticationAttempt(
+                    It.IsAny<DateTime>(), It.IsAny<AuthenticationHistoryType>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_GivenUserDoesNotExist_ExpectFailedResultAndNoAttemptLogged()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => true);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe<IUser>.Nothing);
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe.From(new AuthenticatedUser(Guid.NewGuid())));
+
+            var clock = new Mock<IClock>();
+
+            var handler = new AppMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new AppMfaRequestedCommand();
+
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorCodes.UserNotFound, result.Error.Code);
+            user.Verify(
+                x => x.ProcessPartialSuccessfulAuthenticationAttempt(
+                    It.IsAny<DateTime>(), It.IsAny<AuthenticationHistoryType>()), Times.Never);
+        }
+    }
+
+
+    public class EmailMfaRequestedCommandHandlerTests
+    {
+        [Fact]
+        public async Task Handle_GivenNoUserAppearsToBeAuthenticate_ExpectFailedResultAndNoAttemptLogged()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => true);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe.From(user.Object));
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe<AuthenticatedUser>.Nothing);
+
+            var clock = new Mock<IClock>();
+
+            var handler = new EmailMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new EmailMfaRequestedCommand();
+
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorCodes.UserNotFound, result.Error.Code);
+            user.Verify(
+                x => x.ProcessPartialSuccessfulAuthenticationAttempt(
+                    It.IsAny<DateTime>(), It.IsAny<AuthenticationHistoryType>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_GivenSavingFails_ExpectFailedResult()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => false);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe.From(user.Object));
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe.From(new AuthenticatedUser(Guid.NewGuid())));
+
+            var clock = new Mock<IClock>();
+
+            var handler = new EmailMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new EmailMfaRequestedCommand();
+
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorCodes.SavingChanges, result.Error.Code);
+        }
+
+        [Fact]
+        public async Task Handle_GivenSavingSucceeds_ExpectSuccessfulResult()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => true);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe.From(user.Object));
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe.From(new AuthenticatedUser(Guid.NewGuid())));
+
+            var clock = new Mock<IClock>();
+
+            var handler = new EmailMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new EmailMfaRequestedCommand();
+
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task Handle_GivenUserDoesExist_ExpectSuccessfulResultAndAttemptLogged()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => true);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe.From(user.Object));
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe.From(new AuthenticatedUser(Guid.NewGuid())));
+
+            var clock = new Mock<IClock>();
+
+            var handler = new EmailMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new EmailMfaRequestedCommand();
+
+            await handler.Handle(cmd, CancellationToken.None);
+            user.Verify(
+                x => x.ProcessPartialSuccessfulAuthenticationAttempt(
+                    It.IsAny<DateTime>(), It.IsAny<AuthenticationHistoryType>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Handle_GivenUserDoesNotExist_ExpectFailedResultAndNoAttemptLogged()
+        {
+            var user = new Mock<IUser>();
+            var userRepository = new Mock<IUserRepository>();
+            var unitOfWork = new Mock<IUnitOfWork>();
+            unitOfWork.Setup(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>())).ReturnsAsync(() => true);
+            userRepository.Setup(x => x.UnitOfWork).Returns(unitOfWork.Object);
+            userRepository.Setup(x => x.Find(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(() => Maybe<IUser>.Nothing);
+
+            var currentAuthenticatedUserProvider = new Mock<ICurrentAuthenticatedUserProvider>();
+            currentAuthenticatedUserProvider.Setup(x => x.CurrentAuthenticatedUser)
+                .Returns(Maybe.From(new AuthenticatedUser(Guid.NewGuid())));
+
+            var clock = new Mock<IClock>();
+
+            var handler = new EmailMfaRequestedCommandHandler(
+                userRepository.Object, currentAuthenticatedUserProvider.Object, clock.Object);
+            var cmd = new EmailMfaRequestedCommand();
+
+            var result = await handler.Handle(cmd, CancellationToken.None);
+
+            Assert.True(result.IsFailure);
+            Assert.Equal(ErrorCodes.UserNotFound, result.Error.Code);
+            user.Verify(
+                x => x.ProcessPartialSuccessfulAuthenticationAttempt(
+                    It.IsAny<DateTime>(), It.IsAny<AuthenticationHistoryType>()), Times.Never);
+        }
+    }
+}
