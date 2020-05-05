ec739ee5a6fdf0b5fdc5e6a6d93e0c7de32cb71a
Andrew Davis <andrew@deviouscreation.com>
2020-05-05 08:35:39 +0100

    Adding custom error pages with a middleware for 404 (#58)

---
+++ Source/Stance.Web/Pages/External/NotFound.cshtml.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) DeviousCreation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stance.Web.Pages.External
+{
+    public class NotFound : PageModel
+    {
+        public void OnGet()
+        {
+            this.Response.StatusCode = 200;
+        }
+    }
+}
